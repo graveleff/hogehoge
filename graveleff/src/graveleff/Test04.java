813363204c2e3444c2307443be0abee3f9c576c2
graveleff <graveleff@gmail.com>
2019-04-07 16:39:20 +0900

    gitコマンドの練習 github連携の確認

---
+++ graveleff/src/graveleff/Test04.java
@@ -0,0 +1,15 @@
+package graveleff;
+
+public class Test04 {
+
+	public static void main(String[] args) {
+
+		int[] list = new int[] { 1, 2, 3, 4, 5 };
+
+		for (int data : list) {
+
+			System.out.println(data);
+		}
+
+	}
+}
