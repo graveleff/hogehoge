d04e2ecfa6d1ddf2d1ed3ba2b79830efb5272d50
graveleff <graveleff@gmail.com>
2019-04-10 00:40:33 +0900

    gitHubの練習

---
+++ graveleff/src/graveleff/Practice1_5.java
@@ -0,0 +1,34 @@
+package graveleff;
+
+public class Practice1_5 {
+
+	public static void main(String[] args){
+
+		//練習１－１
+		/*
+		 * ア：コンパイラ
+		 * イ：インタプリタ
+		 * ウ：ソースファイル
+		 * エ：クラスファイル
+		 * オ：JVM
+		 */
+
+		//練習１－２
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+
+		System.out.println(c);
+
+		//練習１－３
+
+		boolean value1 = true;
+		char value2 = '駆';
+		String value21 = "駆";
+		double vale3 = 3.14;
+		long vale4 = 3141545452121L;
+		String vale5 = "ミナトの攻撃・・・";
+
+	}
+
+}
