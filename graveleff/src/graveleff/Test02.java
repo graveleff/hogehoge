f5fbc1e7dad4c0bb534c48f65e2967bda5f4af99
graveleff <graveleff@gmail.com>
2019-04-11 01:05:46 +0900

    今日も練習

--- graveleff/src/graveleff/Test02.java
+++ graveleff/src/graveleff/Test02.java
@@ -18,6 +18,18 @@ public class Test02 {
 
 		}
 
+		int[] test = new int[] { 1, 2, 3, 4, 5, 5, 6, 7, 7, 8 };
+
+		for (int val : test) {
+			System.out.println(val);
+		}
+
+		StringBuilder test2 = new StringBuilder();
+		StringBuilder newStr = test2.append("aaaaa");
+		String newStr2 = newStr.append("bbbbbb").toString();
+
+		System.out.println("newstr = " + newStr2);
+
 	}
 
 }
